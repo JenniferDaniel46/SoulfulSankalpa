cff72eba47eaaa32da992de9a06f775e8ae53d70
Daniel Prejs <danielnprejs@gmail.com>
2024-08-15 16:39:44 -0700

    snap scrolling and hidde menu added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,95 @@
+'use client'
 import Image from "next/image";
+import { useEffect, useState, useContext } from "react";
+import { ScrollContext } from "./layout";
+import YogaLotusSvg from "./components/yogaLotusSvg";
 
 export default function Home() {
+  const [vw, setVw] = useState(700);
+  const [vh, setVh] = useState(700);
+  const [isMobile, setIsMobile] = useState(true);
+  const headerSize = 50;
+  const getWindowSize = () => {
+    console.log("inner: ", window.innerHeight)
+    console.log("outer: ", window.outerHeight)
+    setVh(window.outerHeight);
+    setVw(window.innerWidth);
+    setIsMobile(window.innerWidth <= 768);
+  }
+  useEffect(() => {
+    getWindowSize();
+  }, [])
+  useEffect(() => {
+    window.addEventListener('resize', getWindowSize);
+    return () => {
+      window.removeEventListener('resize', getWindowSize);
+    }
+  }, [vh, vw])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div id="home" onScroll={useContext(ScrollContext)}>
+    <div className="landing screen">
+        <Image
+        alt="Jenn Daniel meditating"
+        src={'/Jenn-outdoors.jpeg'}
+        height={0}
+        width={0}
+        priority={true}
+        loading="eager"
+        sizes="{max-width: 1000px}"
+        style={{height: vh-400, width: 'auto'}}
+        />
+    <div id="title">
+      <h1>
+        Soulful Sankalpa
+      </h1>
+      <h2>
+        Jenn Daniel
+      </h2>
+    </div>
+    <div id="landing-text">
+    Connect with your true goals through Soulful Sankalpa&apos;s unique approach. Utilize yoga, sound therapy, and personalized guidance to align with your deepest desires. Embrace a fulfilling life in harmony with your beliefs. Start your transformative journey today.
+    </div>
+    </div>
+    <div id="soulful-def-page" className="screen hp">
+      <div id="soulful-def">
+        <div id="soulful-def-title">
+          Soulful Sankalpa
+        </div>
+        <div id="soulful-def-content">
+          Using the tools of Yoga, the powerful plant medicine of Cacao and the healing frequencies of sound you will be guided to discover your Sankalpa.
+          Sankalpa is a Sanskrit term that translates to &quot;intention&quot; or &quot;resolve.&quot; It is a deep understanding of one&apos;s true purpose or heartfelt desire. In the practice of yoga, discovering your Sankalpa is a powerful tool for aligning your thoughts, words, and actions with your highest self. By connecting to your Sankalpa, you can set a clear intention and manifest positive change in your life.
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Image
+      alt="group yoga session"
+      src={'/image2.jpeg'}
+      height={0}
+      width={0}
+      loading="lazy"
+      sizes="{max-width: 1000px}"
+      style={{height: "auto", width: vw + 60}}
+      />
+    </div>
+    <div className="screen hp" id="relaxation">
+      <Image
+      alt="sound healing bowl"
+      src={'/bowl.jpeg'}
+      height={0}
+      width={0}
+      loading="lazy"
+      sizes="{min-width: 1000px}"
+      style={{height: "auto", width: vw}}
+      />
+      <div id="relaxationText">
+        Experience relaxation, stress relief, emotional balance with sound healing. Sound healing promotes inner peace. Creates harmony in the body, mind, and spirit through frequencies. Find healing, release, and inspiration for your well-being.
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div id="relaxationSvg">
+        <YogaLotusSvg color="white" fill="white" height={vh -(vw/.75) - headerSize - 10} width={vw - 10}/>
       </div>
-    </main>
+    </div>
+    <div className="screen hp" >
+    </div>
+    </div>
   );
 }
